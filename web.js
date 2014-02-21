9cca5b67d9cc137a6c1c7942ec9aedd18e54b729
daniboomerang <estevez.dani@gmail.com>
2014-02-21 22:48:57 +0100

    Fixing heroku error with web.js.

--- web.js
+++ web.js
@@ -1,14 +1,6 @@
-/*
-This script depends on Node.js and Express
-Install node.js form www.node.org
-
-Install Express by Windows Command Prompt
-Type "npm install Express"
- */
-
-var express = require('express');
+// web.js
+var express = require("express");
 var app = express();
-port = process.argv[2] || 8000;
 
 app.configure(function () {
     app.use(
@@ -16,5 +8,8 @@ app.configure(function () {
         express.static(__dirname) //where your static content is located in your filesystem
     );
 });
-app.listen(port); //the port you want to use
-console.log("Express server running at => http://localhost:" + port + "/\nCTRL + C to shutdown");
+
+var port = Number(process.env.PORT || 5000);
+app.listen(port, function() {
+  console.log("Listening on " + port);
+});
